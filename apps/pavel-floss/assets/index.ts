6a1737b72f32df222b0f6e862f63fe0ab153c11b
hikaaam <andrenuryana@gmail.com>
2023-10-28 10:43:09 +0700

    add Assets

---
+++ apps/pavel-floss/assets/index.ts
@@ -0,0 +1,11 @@
+export const fonts = {
+  Poppins100: require('./fonts/Poppins/Poppins-Thin.ttf'),
+  Poppins200: require('./fonts/Poppins/Poppins-ExtraLight.ttf'),
+  Poppins300: require('./fonts/Poppins/Poppins-Light.ttf'),
+  Poppins400: require('./fonts/Poppins/Poppins-Regular.ttf'),
+  Poppins500: require('./fonts/Poppins/Poppins-Medium.ttf'),
+  Poppins600: require('./fonts/Poppins/Poppins-SemiBold.ttf'),
+  Poppins700: require('./fonts/Poppins/Poppins-Bold.ttf'),
+  Poppins800: require('./fonts/Poppins/Poppins-ExtraBold.ttf'),
+  Poppins900: require('./fonts/Poppins/Poppins-Black.ttf'),
+};
